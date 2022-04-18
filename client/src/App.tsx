2ca01dab1b6b7de103894edeeeee521985f14e21
Your Name <you@example.com>
2022-04-18 22:51:43 +0400

    second commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
-import AuthPage from './pages/AuthPage';
 import Header from './components/Header';
+import RegistrationPage from './pages/Registration';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <div className='app'>
           <Routes>
-            <Route path='/home' element={<AuthPage />} />
+            <Route path='/' element={<RegistrationPage />} />
         </Routes>
       </div>
     </div>
--- client/src/pages/AuthPage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react'
-
-const AuthPage: FC = () => {
-    return (
-        <div>Hello</div>
-    )
-}
-
-export default AuthPage
